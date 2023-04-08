578632fbd0ba4f558371bbb82e41ec95d5e34aee
Đức Pun <ducpun@Macbook-cong-ty.local>
2023-04-08 23:57:25 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,69 @@
+const $ = document.querySelector.bind(document);
+const $all = document.querySelectorAll.bind(document);
+//===== CONSTANT ELEMENT ==== //
+const btnTextPrice = $("#btnTextPrice");
+const btnCollapseOne = $("#btnCollapseOne");
+const btnSpin = $("#btn-spin");
+const toast = $("#liveToast");
+const myModal = new bootstrap.Modal($("#exampleModal"));
+//===== END CONSTANT ELEMENT  REGION==== //
+
+//=====INIT REGION ==== //
+class State {
+  constructor(element, initState, callback = () => {}) {
+    this.element = element;
+    this.state = initState;
+    this.element.setAttribute("data-state", initState);
+    this.callback = callback;
+  }
+  setState(newState) {
+    this.state = newState;
+    this.subcriber();
+    this.element.setAttribute("data-state", newState);
+  }
+  subcriber() {
+    this.callback(this.state);
+  }
+}
+function handleSpin(state) {
+  const status = {
+    pendding: "pendding",
+    spin: "spin",
+  };
+  const prevState = btnSpin.getAttribute("data-state");
+  btnSpin.classList.remove(prevState);
+  btnSpin.classList.add(status[state]);
+}
+
+// this function is fake api call
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toast);
+const btnTextPriceState = new State(btnTextPrice, false);
+const btnSpinState = new State(btnSpin, "pendding", handleSpin);
+//===== END INIT REGION ==== //
+
+//====== MAIN LOGIC =========//
+const listGroupItemPrice = $all(".list-group-item");
+
+/* Hanlde change text when click price */
+listGroupItemPrice.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    const text = e.target.innerHTML;
+    btnTextPrice.innerHTML = text;
+    btnTextPriceState.setState(true);
+  });
+});
+btnSpin.addEventListener("click", () => {
+  if (!btnTextPriceState.state) {
+    return toastBootstrap.show();
+  }
+  btnSpinState.setState("spin");
+  sleep(3000).then(() => {
+    btnSpinState.setState("pendding");
+    // handle logic show modal here
+    myModal.show();
+  });
+});
